0dd6593a2ede1df706222e09515379e22da62245
Kay Zheng <l04m33@gmail.com>
2013-10-16 17:45:36 +0800

    Remove demo_protocol.hrl

--- priv/demo/demo_protocol.erl
+++ priv/demo/demo_protocol.erl
@@ -1,5 +1,21 @@
 -module(demo_protocol).
 
 -include("rec_to_msgpack.hrl").
--include("demo_protocol.hrl").
+
+-record(pt_time_flag, {
+        type  = 0       :: mp_int(),
+        value = 0       :: mp_int()
+       }).
+
+-record(pt_time_entry, {
+        time_zone = 0   :: mp_int(),
+        time = 0        :: mp_int(),
+        flags = []      :: mp_array(#pt_time_flag{})
+       }).
+
+-record(pt_time_i, {}).
+-record(pt_time_o, {
+        times = []  :: mp_array(#pt_time_entry{}),
+        flag  = 0   :: mp_int()
+       }).
 
